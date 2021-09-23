2e5d8080e6903fa02b47f1a7b9c52fed3a475e57
Teslim Olunlade <tolunlade@outlook.com>
2021-09-23 16:53:11 -0400

    Fixed improper main path in conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while !(mysqladmin -uroot ping); do sleep 3; echo "waiting for mysql ..."; done
-./custom.d/initdb.sh
-/etc/init.d/lighttpd start
+./custom.d/db-scripts.d/m:lxr:lxr_.sh
 ./genxref --url=http://localhost --allversions
+/etc/init.d/lighttpd start
 exec "$@"
