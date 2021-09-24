2e56ce1b78ff8d2f0f5e9370c9b7f3cebcdd19ae
Teslim Olunlade <tolunlade@outlook.com>
2021-09-23 23:17:44 -0400

    Minor improvments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 while !(mysqladmin -uroot ping); do sleep 3; echo "waiting for mysql ..."; done
 ./custom.d/db-scripts.d/m:lxr:lxr_.sh
-./genxref --url=http://localhost --allversions
+chmod -R 777 /opt/src
 /etc/init.d/lighttpd start
+./genxref --url=http://localhost --version=main
 exec "$@"
